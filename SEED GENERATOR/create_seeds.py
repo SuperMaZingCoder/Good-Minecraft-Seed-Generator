6923fb8c6e9643712db851e9235771eefb0d741e
CodeForeverAndEver <55718659+CodeForeverAndEver@users.noreply.github.com>
2020-03-22 19:13:50 -0400

    Added a generator for seeds

---
+++ SEED GENERATOR/create_seeds.py
@@ -0,0 +1,8 @@
+# Generate List of 100 Seeds
+
+import random
+
+with open('seeds.txt', 'w') as f:
+    for i in range(1000):
+        f.write(str(random.randrange(-9223372036854775808,
+                                     9223372036854775807)) + '\n')
