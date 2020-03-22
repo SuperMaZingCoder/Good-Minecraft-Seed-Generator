8afd21bcc7dad179fc63715b34d3c491b54f1269
CodeForeverAndEver <55718659+CodeForeverAndEver@users.noreply.github.com>
2020-03-22 19:14:05 -0400

    Update create_seeds.py

--- SEED GENERATOR/create_seeds.py
+++ SEED GENERATOR/create_seeds.py
@@ -3,6 +3,6 @@
 import random
 
 with open('seeds.txt', 'w') as f:
-    for i in range(1000):
+    for i in range(100):
         f.write(str(random.randrange(-9223372036854775808,
                                      9223372036854775807)) + '\n')
