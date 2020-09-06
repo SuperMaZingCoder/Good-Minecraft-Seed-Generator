4c56ec4dc5e362b40c0add7594ad0ca0da2b4352
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2020-09-06 19:24:39 +0200

    cleaned import up

--- SEED GENERATOR/create_seeds.py
+++ SEED GENERATOR/create_seeds.py
@@ -1,8 +1,8 @@
 # Generate List of 100 Seeds
 
-import random
+from random import randrange
 
 with open('seeds.txt', 'w') as f:
     for i in range(100):
-        f.write(str(random.randrange(-9223372036854775808,
+        f.write(str(randrange(-9223372036854775808,
                                      9223372036854775807)) + '\n')
